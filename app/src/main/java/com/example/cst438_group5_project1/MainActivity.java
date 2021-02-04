f2c2e62b1f29e4dcc898c11ae08642b317965420
Adrian Ortiz <adortiz@csumb.edu>
2021-02-04 13:05:16 -0800

    Created Login Page

--- app/src/main/java/com/example/cst438_group5_project1/MainActivity.java
+++ app/src/main/java/com/example/cst438_group5_project1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cst438_group5_project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createAccount(View v){
+        if(v.getId() == R.id.enter_button){
+            EditText tempNewUserName;
+            EditText tempNewPassWord;
+            String newUserName;
+            String newPassWord;
+
+            tempNewUserName = (EditText) findViewById(R.id.new_username);
+            tempNewPassWord = (EditText) findViewById(R.id.new_password);
+
+            newUserName = tempNewUserName.getText().toString();
+            newPassWord = tempNewPassWord.getText().toString();
+        }
+
     }
 }
