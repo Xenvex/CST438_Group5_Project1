e7122b01d2a45d56e76097ec5aa8fa6ad3c2d252
Adrian Ortiz <adortiz@csumb.edu>
2021-02-10 19:41:13 -0800

    Fixed the code to create account correctly

--- app/src/main/java/com/example/cst438_group5_project1/CreateNewAccount.java
+++ app/src/main/java/com/example/cst438_group5_project1/CreateNewAccount.java
@@ -3,6 +3,7 @@ package com.example.cst438_group5_project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,18 +16,20 @@ public class CreateNewAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new_account);
 
-        TextView title = (TextView) findViewById(R.id.title);
-        title.setText("Welcome, create a new account below");
+    }
 
-        EditText tempNewUserName;
-        EditText tempNewPassWord;
-        String newUserName;
-        String newPassWord;
+    public void createAccount(View v){
+        if(v.getId() == R.id.enter_button){
+            EditText tempNewUserName;
+            EditText tempNewPassWord;
+            String newUserName;
+            String newPassWord;
 
-        tempNewUserName = (EditText) findViewById(R.id.new_username);
-        tempNewPassWord = (EditText) findViewById(R.id.new_password);
+            tempNewUserName = (EditText) findViewById(R.id.new_username);
+            tempNewPassWord = (EditText) findViewById(R.id.new_password);
 
-        newUserName = tempNewUserName.getText().toString();
-        newPassWord = tempNewPassWord.getText().toString();
+            newUserName = tempNewUserName.getText().toString();
+            newPassWord = tempNewPassWord.getText().toString();
+        }
     }
 }
