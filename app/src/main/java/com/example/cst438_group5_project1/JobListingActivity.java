4cb35a26f6ed45e1b3441ccce5953f191773d24e
Xenvex <matthewbreyes@gmail.com>
2021-02-12 20:21:59 -0800

    Bug Fixing
    Fixed a Bug where Internet Permissions weren't working, which prevented any API retrieval

--- app/src/main/java/com/example/cst438_group5_project1/JobListingActivity.java
+++ app/src/main/java/com/example/cst438_group5_project1/JobListingActivity.java
@@ -35,5 +35,40 @@ public class JobListingActivity extends AppCompatActivity {
         //network request
         Call<List<Post>> call = jsonPlaceHolderAPI.getPosts();
 
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                Log.i("Log Message", "Response is Happening");
+                if (!response.isSuccessful()){
+                    //if not successful, this shows us what went wrong
+                    textViewResult.setText("Code: " + response.code());
+                    return; //Prevents us from doing any operations with a NULL response
+                }
+
+                List<Post> posts = response.body();
+                //Data we want from servers
+                //Returns a list of posts from the JSON array
+
+                for (Post post : posts) {
+                    Log.i("Log Message", "Retrieval Successful!");
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            //If something went wrong when communicating with the server
+            //or something went wrong with the response
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.i("Log Message", "Response Failed");
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
     }
 }
