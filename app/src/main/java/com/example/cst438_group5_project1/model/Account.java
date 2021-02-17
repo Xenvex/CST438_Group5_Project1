40478dab9e0e96e208e5bb56d3fb27597281d19b
Adrian Ortiz <adortiz@csumb.edu>
2021-02-16 18:01:12 -0800

    Created the Account class

---
+++ app/src/main/java/com/example/cst438_group5_project1/model/Account.java
@@ -0,0 +1,49 @@
+package com.example.cst438_group5_project1.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "accounts")
+public class Account {
+
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String userName;
+
+    @ColumnInfo(name = "password")
+    private String userPassword;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Account(String userName, String userPassword) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+
+}
