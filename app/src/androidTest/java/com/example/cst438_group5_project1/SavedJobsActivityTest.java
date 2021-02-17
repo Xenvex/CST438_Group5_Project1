30c2cb3ccebf7e4b222a04af5740954b7ed0d722
Christian Sumares <christiansumares@gmail.com>
2021-02-16 17:03:05 -0800

    Placeholder instrumented test for saved jobs activity

---
+++ app/src/androidTest/java/com/example/cst438_group5_project1/SavedJobsActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.cst438_group5_project1;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class SavedJobsActivityTest {
+    @Test
+    public void retrieveSavedJobsTest() {
+        // TODO
+        // Add some saved jobs to the database
+        // Retrieve them
+        // Make sure all of the correct ones have been retrieved
+    }
+}
