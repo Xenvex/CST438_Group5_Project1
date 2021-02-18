1d633a782cabe6dbdd92c6c2bc9a6aba1a3a0b20
Anthony Perez <54605746+anthonyperez992@users.noreply.github.com>
2021-02-18 13:48:34 -0800

    debugging

--- app/src/main/java/com/example/cst438_group5_project1/EditProfile.java
+++ app/src/main/java/com/example/cst438_group5_project1/EditProfile.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.cst438_group5_project1.model.Account;
+import com.example.cst438_group5_project1.model.AccountDao;
 import com.example.cst438_group5_project1.model.AccountDatabase;
 
 public class EditProfile extends AppCompatActivity {
@@ -20,6 +22,8 @@ public class EditProfile extends AppCompatActivity {
     Button save;
 
     Account editingAcc;
+    private AccountDao accountDAO;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +34,21 @@ public class EditProfile extends AppCompatActivity {
         passWord = findViewById(R.id.pWord);
         accountInt = findViewById(R.id.jobInt);
 
+        cancel = findViewById(R.id.cancel);
+        save = findViewById(R.id.saveChanges);
+
         Bundle extras = getIntent().getExtras();
         String uName = extras.getString("username", "JohnDoe");
 
         editingAcc = AccountDatabase.getInstance(this).account().getAccountInfo(uName);
+        accountDAO = AccountDatabase.getInstance(this).account();
+
 
         userName.setText(editingAcc.getUserName());
         passWord.setText(editingAcc.getUserPassword());
 //        accountInt.setText();
         cancel.setOnClickListener(v -> cancelEdit(uName));
-        save.setOnClickListener(v -> saveEdit(editingAcc, userName.getText().toString(), passWord.getText().toString()));
+        save.setOnClickListener(v -> saveEdit(userName.getText().toString(), passWord.getText().toString()));
     }
 
     public void cancelEdit(String userName){
@@ -48,10 +57,11 @@ public class EditProfile extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void saveEdit(Account account, String newUser, String newPass){
-        account.setUserName(newUser);
-        account.setUserPassword(newPass);
-
+    public void saveEdit(String newUser, String newPass){
+        Log.i("accountcontents", "Account: " + editingAcc);
+        editingAcc.setUserName(newUser);
+        editingAcc.setUserPassword(newPass);
+        accountDAO.update(editingAcc);
         Intent intent = new Intent(this, ViewProfile.class);
         intent.putExtra("username", newUser);
         startActivity(intent);
